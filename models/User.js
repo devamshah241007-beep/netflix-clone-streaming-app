358591e611656d49eae0059a2b2f5e977ad328c0
devamshah241007-beep <devamshah241007@gmail.com>
2025-12-02 18:39:32 +0530

    Add User model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  subscription: {
+    type: String,
+    enum: ['basic', 'standard', 'premium'],
+    default: 'basic'
+  },
+  profiles: [{
+    name: String,
+    avatar: String,
+    isKids: { type: Boolean, default: false }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
